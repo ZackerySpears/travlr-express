e82a951f46ba73a3d7239b297e0aeddcd41e656b
Zackery Spears <zackery.spears97@gmail.com>
2026-04-15 12:06:42 -0500

    Complete Module Seven authentication and JWT security

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -6,8 +6,8 @@ const auth = require('../jwt');
 router.get('/trips', ctrlTrips.tripsList);
 router.get('/trips/:tripId', ctrlTrips.tripsFindById);
 
-router.post('/trips', ctrlTrips.tripsAddTrip);
-router.put('/trips/:tripId', ctrlTrips.tripsUpdateTrip);
-router.delete('/trips/:tripId', ctrlTrips.tripsDeleteTrip);
+router.post('/trips', auth, ctrlTrips.tripsAddTrip);
+router.put('/trips/:tripId', auth, ctrlTrips.tripsUpdateTrip);
+router.delete('/trips/:tripId', auth, ctrlTrips.tripsDeleteTrip);
 
 module.exports = router;
